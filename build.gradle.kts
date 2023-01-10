b90790a2bca655d36fac1b48fcc3810411a7dc7c
ivlukin <Lukin2101>
2023-01-10 20:24:34 +0300

    m1l1 home work

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.21"
+}
+
+group = "ru.otus.otuskotlin.marketplace"
+version = "1.0.0-SNAPSHOT"
+
+
+subprojects {
+    group = rootProject.group
+    version = rootProject.version
+    repositories {
+        mavenCentral()
+    }
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,5 @@
+
+rootProject.name = "okmarketplace-2023"
+
+include("m1l1-quickstart")
+
